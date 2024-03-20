4cd625d8725d72b6e60d5d52a7d549b123186d96
MrWyss-MSFT <mawyss@microsoft.com>
2024-03-20 11:12:08 +0100

    adding scripts to the repo

---
+++ Scripts/Autopilot/Copy-AutoPilotHashToClipboard.ps1
@@ -0,0 +1,2 @@
+#requires -RunAsAdministrator
+(Get-CimInstance -Namespace root/cimv2/mdm/dmmap -Class MDM_DevDetail_Ext01 -Filter "InstanceID='Ext' AND ParentID='./DevDetail'").DeviceHardwareData | clip
