df9f9f1d6f12a7a96f6072e4a1f93fab4bfe8b27
MrWyss-MSFT <mawyss@microsoft.com>
2024-03-21 11:12:19 +0100

    Fix spelling of "requires_elevated" variable in GistIntuneScriptTrigger.ps1

--- GistIntuneScriptTrigger.ps1
+++ GistIntuneScriptTrigger.ps1
@@ -9,35 +9,35 @@ $GistCatalog = [ordered]@{
         url                = "https://gist.githubusercontent.com/MrWyss-MSFT/c52f0a4567cba24e9ac8a38416a05bac/raw/cffeca3256e5c4c31fdd8b1cbaa8b86ced9833f5/Get-Win32AppsOrder.ps1"
         description        = "Reads the IntuneManagementExtension.log and returns a ordered list of the Win32Apps"
         author             = "MrWyss-MSFT"
-        requires_eleveated = $false
+        requires_elevated = $false
     }
     'Get-CoMgmtWL.ps1'                  = @{
         category           = "CoMgmt"
         url                = "https://gist.githubusercontent.com/MrWyss-MSFT/228ed9f3dd2b67077790a3bef98442d5/raw/51699848761c0625991ff1374f227f945e8f42cd/Get-CoMgmtWL.ps1"
         description        = "Returns if CoManagement is enabled and how the workloads are configured"
         author             = "MrWyss-MSFT"
-        requires_eleveated = $true
+        requires_elevated = $true
     }
     'Get-AutopilotAndESPProgress.ps1'   = @{
         category           = "Autopilot"
         url                = "https://gist.githubusercontent.com/MrWyss-MSFT/d511d655f55762233a1d442c24d584f6/raw/6a508ad8ca7f7f5206365f699b9a8c25a01b2dd5/Get-AutopilotAndESPProgress.ps1"
         description        = "Script to view the Autopilot and ESP Progress"
         author             = "MrWyss-MSFT"
-        requires_eleveated = $false
+        requires_elevated = $false
     }
     'Copy-AutoPilotHashToClipboard.ps1' = @{
         category           = "Autopilot"
         url                = "https://gist.githubusercontent.com/MrWyss/b83733e8378139af6032381359e00803/raw/7f87d17406e7e120be25c6c35fe1fc154233f17e/Copy-AutoPilotHashToClipboard.ps1"
         description        = "Copy the Autopilot Hash to the clipboard"
         author             = "MrWyss"
-        requires_eleveated = $true
+        requires_elevated = $true
     }
     'New-IntuneRegistryFavorites.ps1'   = @{
         category           = "Intune"
         url                = "https://gist.githubusercontent.com/MrWyss-MSFT/500b2270b0b23b2fdc9ddc78092c355d/raw/9e3237f26674e2b0ad69b585849797fb731da0f2/New-IntuneRegistryFavorites.ps1"
         description        = "Create Registry Favorites for Intune and Autopilot Settings"
         author             = "MrWyss-MSFT"
-        requires_eleveated = $true
+        requires_elevated = $true
     }
 }
 
@@ -98,8 +98,10 @@ $selection = Read-Host "Select a script by number"
 if ($selection -match '^\d+$' -and $selection -ge 1 -and $selection -le $GistCatalog.Count) {
     $scriptTitle = $GistCatalog.Keys[([int]$selection - 1)]
     $scriptUri = $GistCatalog[([int]$selection - 1)].url
-    Write-host "You selected: $scriptTitle" -ForegroundColor Green
-    Write-host "URL: $scriptUri" -ForegroundColor Green
+    $author = $GistCatalog[([int]$selection - 1)].author
+    $requires_elevated = $GistCatalog[([int]$selection - 1)].requires_elevated
+    $ScriptInfo = "`n  Name: {0}`n  Author: {2}`n  Requires elevated rights: {3}`n  URL: {4}" -f $scriptTitle, $scriptUri, $author, $requires_elevated, $scriptUri
+    Write-host "You selected: $ScriptInfo" -ForegroundColor Green
     $decision = $Host.UI.PromptForChoice('Run the Script', 'Are you sure you want to proceed?', @('&Yes'; '&No'), 0)
     if ($decision -eq 0) {
         try {
